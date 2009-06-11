57d87dc443adf09f750731207028e5f84dd8df37
Michael Hale <mikehale@gmail.com>
2009-06-11 17:16:05 -0400

    refactor mock file system

--- features/support/mock_file_system.rb
+++ features/support/mock_file_system.rb
@@ -1,18 +1,26 @@
 module MockFileSystem
 
   def mock_dir(path)
+    # Make sure any calls we don't care about get to the actual filesystem.
     stub.proxy(File).directory? { |result| result }
     stub.proxy(File).exist? { |result| result }
     stub.proxy(File).stat { |result| result }
 
-    mock(Dir).mkdir(path).at_least(1) {0}
-    mock(File).exist?(path).times(any_times) {true}
-    mock(File).directory?(path).times(any_times) {true}
-    stub(cstats = Object.new)
-    stub(cstats).uid {500}
-    stub(cstats).gid {500}
-    stub(cstats).mode {0755}
-    mock(File).stat(path).times(any_times) {cstats}
+    mock(Dir).mkdir(path).at_least(1) { 0 }
+    mock(File) do |f|
+      f.exist?(path).times(any_times) { true }
+      f.directory?(path).times(any_times) { true }
+      f.stat(path).times(any_times) { mock_stats }
+    end
+    
+  end
+  
+  def mock_stats
+    stub(stats = Object.new)
+    stub(stats).uid { 500 }
+    stub(stats).gid { 500 }
+    stub(stats).mode { 0755 }
+    stats
   end
   
 end
